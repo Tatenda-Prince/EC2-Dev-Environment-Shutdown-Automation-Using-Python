11cd80be5874347ff0652f1d740e846752013593
Tatenda Prince Moyo <166498977+Tatenda-Prince@users.noreply.github.com>
2025-02-14 20:22:16 +0200

    Update stopping_Instances.py

--- stopping_Instances.py
+++ stopping_Instances.py
@@ -16,24 +16,24 @@ def lambda_handler(event, context):
         ]
     )
     
-    instances_to_stop = []
+    instances_to_terminate = []
     
     for reservation in response['Reservations']:
         for instance in reservation['Instances']:
-            instances_to_stop.append(instance['InstanceId'])
+            instances_to_terminate.append(instance['InstanceId'])
     
-    if instances_to_stop:
-        # Stop the instances
-        ec2.stop_instances(InstanceIds=instances_to_stop)
-        print(f"Stopped instances: {instances_to_stop}")
+    if instances_to_terminate:
+        # Terminate the instances
+        ec2.terminate_instances(InstanceIds=instances_to_terminate)
+        print(f"Terminated instances: {instances_to_terminate}")
         
         # Send notification
-        sns_topic_arn = 'arn:aws:sns:us-east-1:664418964175:InstanceStopNotification'
-        message = f"The following instances with tag {tag_key}={tag_value} were stopped: {instances_to_stop}"
+        sns_topic_arn = 'arn:aws:sns:us-east-1:664418964175:InstanceTerminateTopic'
+        message = f"The following instances with tag {tag_key}={tag_value} were terminated: {instances_to_terminate}"
         sns.publish(TopicArn=sns_topic_arn, Message=message)
         print(f"Notification sent: {message}")
     else:
-        print("No instances to stop.")
+        print("No instances to terminate.")
     
     return {
         'statusCode': 200,
